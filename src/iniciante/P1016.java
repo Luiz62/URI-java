eee865f101277fb2ce7328853dada58a44c03e87
Luiz62 <luiztiagonascimento21@gmail.com>
2022-01-17 18:26:52 -0300

    exercicio 1016

---
+++ src/iniciante/P1016.java
@@ -0,0 +1,21 @@
+package iniciante;
+
+import java.io.IOException;
+
+/**
+ * IMPORTANT:
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class P1016 {
+
+    public static void main(String[] args) throws IOException {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int km = sc.nextInt();
+
+        System.out.printf("%d minutos%n", km * 2);
+
+    }
+
+}
